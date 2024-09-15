e2f1d9f07dddeb19340c4709efba53b2c2809441
arwinGhaderi <arwinghaderi81@gmail.com>
2024-09-15 12:10:59 +0330

    perf:Creating a save and get function in local storage in the utilsfolder

--- js/func/utils.js
+++ js/func/utils.js
@@ -1,18 +1,27 @@
 import { products } from "../db/data.js";
 
-
-
-
 const paginationCalculations = (numberRow, currentPage, resultShowProducts) => {
     let indexEnd, indexStart
     let copyProducts = [...products]
 
     indexEnd = numberRow * currentPage
     indexStart = indexEnd - numberRow
-    
+
     resultShowProducts.innerHTML = "Showing" + indexStart + "-- " + indexEnd + " of" + products.length + " results"
     let paginationProducts = copyProducts.slice(indexStart, indexEnd)
 
     return paginationProducts
 }
-export { paginationCalculations }
+
+
+const saveToLocalStorage = (key, value) => {
+    return localStorage.setItem(key, JSON.stringify(value))
+}
+
+const getFromLocalStorage = (key) => {
+    let value = JSON.parse(localStorage.getItem(key))
+    return value
+}
+
+
+export { paginationCalculations, saveToLocalStorage, getFromLocalStorage }

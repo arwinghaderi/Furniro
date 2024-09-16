920b32d4341272097119cc5cfe3a74bb043a0f38
arwinGhaderi <arwinghaderi81@gmail.com>
2024-09-16 17:59:14 +0330

    refactor :Changes in the logic of two types of product structure in the filter section

--- js/func/shared.js
+++ js/func/shared.js
@@ -1,12 +1,12 @@
 function addingProductsTemplate(products, productsStructure, productsWrapper) {
-    productsWrapper.innerHTML = '';
     // const content = element.innerHTML;
     console.log(productsStructure);
     if (productsStructure === 'row') {
+        productsWrapper.innerHTML = '';
         products.forEach(function (product) {
             let discountTemplate = `<div class="Discount-product"><span class="Discount-product__number">${product.discountPercent}%</span></div>`
             let newTemplate = ` <div class="Discount-product Discount-product--green "><span class="Discount-product__number">NEW</span></div>`
-            let discountPrice = `</span > <span class="product-discountPrice">Rp ${product.price}</span>`
+            let discountPrice = `</span > <span class="product-discountPrice ">Rp ${product.price}</span>`
 
             let price = +product.price
             let discountPercent = +product.discountPercent
@@ -14,7 +14,7 @@ function addingProductsTemplate(products, productsStructure, productsWrapper) {
             let total = price - totalDiscount
 
             productsWrapper.insertAdjacentHTML("beforeend", `<div class="col col-lg-3 product-main-box product-main-box__shop product-main-box__shop--show "><div class="product-box"  id="discount"><div   
-     class="img-box-overlay"><img class="product__img" src="${product.img}" alt="product img"><div class="product-overlay"> 
+     class="img-box-overlay"><img class="product__img product__img-col" src="${product.img}" alt="product img"><div class="product-overlay">
      <div class="box-add-btn-cart"><a href="../Pages/product.html?id= ${product.id} " class="box-add-btn-cart__text">Details Product</a></div> 
      <div class="product-overlay__options"> 
      <div class="option-overlay"> 
@@ -28,6 +28,7 @@ function addingProductsTemplate(products, productsStructure, productsWrapper) {
     </div> </div></div></div>  `)
         })
     } else {
+        productsWrapper.innerHTML = '';
         products.forEach(function (product) {
             let discountTemplate = `<div class="Discount-product"><span class="Discount-product__number">${product.discountPercent}%</span></div>`
             let newTemplate = ` <div class="Discount-product Discount-product--green "><span class="Discount-product__number">NEW</span></div>`
@@ -38,8 +39,8 @@ function addingProductsTemplate(products, productsStructure, productsWrapper) {
             let totalDiscount = (price * discountPercent) / 100
             let total = price - totalDiscount
 
-            productsWrapper.insertAdjacentHTML("beforeend", `<div class="col col-lg-12 product-main-box product-main-box__shop product-main-box__shop--show "><div class="product-box"  id="discount"><div   
-     class="img-box-overlay"><img class="product__img" src="${product.img}" alt="product img"><div class="product-overlay"> 
+            productsWrapper.insertAdjacentHTML("beforeend", `<div class="col col-lg-12 product-main-box product-main-box__shop product-main-box__shop--show "><div class="product-box product-box-col"  id="discount"><div   
+     class="img-box-overlay img-box-overlay-col "><img class="product__img" src="${product.img}" alt="product img"><div class="product-overlay">
      <div class="box-add-btn-cart"><a href="../Pages/product.html?id= ${product.id} " class="box-add-btn-cart__text">Details Product</a></div> 
      <div class="product-overlay__options"> 
      <div class="option-overlay"> 
@@ -48,7 +49,7 @@ function addingProductsTemplate(products, productsStructure, productsWrapper) {
      <i class="fas fa-exchange-alt  icon"></i>
      <span class="option-overlay__text">Compare</span></div> 
      <div class="option-overlay"><i class="fa-regular fa-heart icon"></i>
-     <span class="option-overlay__text">Like</span></div></div></div> ${product.discount ? discountTemplate : product.newProduct ? newTemplate : ""}</div><div class="product-Introductions"><h3 class="product-name">  ${product.productName}  </h3><p class="product-Introduction"> ${product.productIntroduction}   </p><div class="product-price-box"> 
+     <span class="option-overlay__text">Like</span></div></div></div> ${product.discount ? discountTemplate : product.newProduct ? newTemplate : ""}</div><div class="product-Introductions product-Introductions-col "><h3 class="product-name product-name-col ">  ${product.productName}  </h3><p class="product-Introduction product-Introduction-col"> ${product.productIntroduction}   </p><div class="product-price-box"> 
     <span class="product-price"> Rp  ${total.toLocaleString("en")}   ${product.discount ? discountPrice : ""}
     </div> </div></div></div>  `)
         })

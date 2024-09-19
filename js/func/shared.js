a6fec0429f6d42f75de9c9c32d30bec06e95b6a8
arwinGhaderi <arwinghaderi81@gmail.com>
2024-09-19 17:16:23 +0330

    perf : Functional conversion of adding products template to composite function

--- js/func/shared.js
+++ js/func/shared.js
@@ -1,24 +1,37 @@
 
-function addingProductsTemplate(products, productsStructure, productsWrapper) {
-    const fragment = document.createDocumentFragment();
-    let price, discountPercent, totalDiscount, total, discountTemplate, newTemplate, discountPrice, element
+let price, discountPercent, totalDiscount, total, discountTemplate, newTemplate, discountPrice, element
+const fragment = document.createDocumentFragment();
 
+const addingProductsTemplate = (products, productsStructure, productsWrapper) => {
+    let fragment
     if (productsStructure === 'row') {
         productsWrapper.innerHTML = '';
         products.forEach(function (product) {
-            element = document.createElement("div")
-            element.className = `col col-lg-3 product-main-box product-main-box__shop product-main-box__shop--show `
+            fragment = addingProductsTemplateRow(product)
+        })
+    } else {
+        productsWrapper.innerHTML = '';
+        products.forEach(function (product) {
+            fragment = addingProductsTemplateCol(product)
+        })
+    }
+    productsWrapper.append(fragment)
+}
 
-            discountTemplate = `<div class="Discount-product"><span class="Discount-product__number">${product.discountPercent}%</span></div>`
-            newTemplate = ` <div class="Discount-product Discount-product--green "><span class="Discount-product__number">NEW</span></div>`
-            discountPrice = `</span > <span class="product-discountPrice ">Rp ${product.price}</span>`
+const addingProductsTemplateRow = (product) => {
+    element = document.createElement("div")
+    element.className = `col col-lg-3 product-main-box product-main-box__shop product-main-box__shop--show `
 
-            price = +product.price
-            discountPercent = +product.discountPercent
-            totalDiscount = (price * discountPercent) / 100
-            total = price - totalDiscount
+    discountTemplate = `<div class="Discount-product"><span class="Discount-product__number">${product.discountPercent}%</span></div>`
+    newTemplate = ` <div class="Discount-product Discount-product--green "><span class="Discount-product__number">NEW</span></div>`
+    discountPrice = `</span > <span class="product-discountPrice ">Rp ${product.price}</span>`
 
-            element.innerHTML = `<div class="product-box"  id="discount"><div   
+    price = +product.price
+    discountPercent = +product.discountPercent
+    totalDiscount = (price * discountPercent) / 100
+    total = price - totalDiscount
+
+    element.innerHTML = `<div class="product-box"  id="discount"><div   
      class="img-box-overlay"><img class="product__img product__img-col" src="${product.img}" alt="product img"><div class="product-overlay">
      <div class="box-add-btn-cart"><a href="../Pages/product.html?id= ${product.id} " class="box-add-btn-cart__text">Details Product</a></div> 
      <div class="product-overlay__options"> 
@@ -31,24 +44,25 @@ function addingProductsTemplate(products, productsStructure, productsWrapper) {
      <span class="option-overlay__text">Like</span></div></div></div> ${product.discount ? discountTemplate : product.newProduct ? newTemplate : ""}</div><div class="product-Introductions"><h3 class="product-name">  ${product.productName}  </h3><p class="product-Introduction"> ${product.productIntroduction}   </p><div class="product-price-box"> 
     <span class="product-price"> Rp  ${total.toLocaleString("en")}   ${product.discount ? discountPrice : ""}
     </div> </div></div>`
-            fragment.append(element)
-        })
-    } else {
-        productsWrapper.innerHTML = '';
-        products.forEach(function (product) {
-            element = document.createElement("div")
-            element.className = `col col-lg-12 product-main-box product-main-box__shop product-main-box__shop--show `
-            
-            discountTemplate = `<div class="Discount-product"><span class="Discount-product__number">${product.discountPercent}%</span></div>`
-            newTemplate = ` <div class="Discount-product Discount-product--green "><span class="Discount-product__number">NEW</span></div>`
-            discountPrice = `</span > <span class="product-discountPrice">Rp ${product.price}</span>`
+    fragment.append(element)
 
-            price = +product.price
-            discountPercent = +product.discountPercent
-            totalDiscount = (price * discountPercent) / 100
-            total = price - totalDiscount
+    return fragment
+}
 
-            element.innerHTML = `<div class="product-box product-box-col"  id="discount"><div   
+const addingProductsTemplateCol = (product) => {
+
+    element = document.createElement("div")
+    element.className = `col col-lg-12 product-main-box product-main-box__shop product-main-box__shop--show `
+    discountTemplate = `<div class="Discount-product"><span class="Discount-product__number">${product.discountPercent}%</span></div>`
+    newTemplate = ` <div class="Discount-product Discount-product--green "><span class="Discount-product__number">NEW</span></div>`
+    discountPrice = `</span > <span class="product-discountPrice">Rp ${product.price}</span>`
+
+    price = +product.price
+    discountPercent = +product.discountPercent
+    totalDiscount = (price * discountPercent) / 100
+    total = price - totalDiscount
+
+    element.innerHTML = `<div class="product-box product-box-col"  id="discount"><div   
      class="img-box-overlay img-box-overlay-col "><img class="product__img" src="${product.img}" alt="product img"><div class="product-overlay">
      <div class="box-add-btn-cart"><a href="../Pages/product.html?id= ${product.id} " class="box-add-btn-cart__text">Details Product</a></div> 
      <div class="product-overlay__options"> 
@@ -61,12 +75,10 @@ function addingProductsTemplate(products, productsStructure, productsWrapper) {
      <span class="option-overlay__text">Like</span></div></div></div> ${product.discount ? discountTemplate : product.newProduct ? newTemplate : ""}</div><div class="product-Introductions product-Introductions-col "><h3 class="product-name product-name-col ">  ${product.productName}  </h3><p class="product-Introduction product-Introduction-col"> ${product.productIntroduction}   </p><div class="product-price-box"> 
     <span class="product-price"> Rp  ${total.toLocaleString("en")}   ${product.discount ? discountPrice : ""}
     </div> </div></div>`
-            fragment.append(element)
-        })
-    }
-    productsWrapper.append(fragment)
-}
+    fragment.append(element)
 
+    return fragment
+}
 
 const productsSorting = (products, target) => {
     let productsFilter = []

f73075e30a46d0a9409c0823cdf0bbe576d6eb12
arwinGhaderi <arwinghaderi81@gmail.com>
2024-09-19 16:36:44 +0330

    perf : Changing insertAdjacentHTML to fragment in loop for better performance

--- js/func/shared.js
+++ js/func/shared.js
@@ -1,8 +1,12 @@
+
 function addingProductsTemplate(products, productsStructure, productsWrapper) {
     productsWrapper.innerHTML = ""
+    const fragment = document.createDocumentFragment();
     if (productsStructure === 'row') {
         productsWrapper.innerHTML = '';
         products.forEach(function (product) {
+            const element = document.createElement("div")
+            element.className = `col col-lg-3 product-main-box product-main-box__shop product-main-box__shop--show `
             const discountTemplate = `<div class="Discount-product"><span class="Discount-product__number">${product.discountPercent}%</span></div>`
             const newTemplate = ` <div class="Discount-product Discount-product--green "><span class="Discount-product__number">NEW</span></div>`
             const discountPrice = `</span > <span class="product-discountPrice ">Rp ${product.price}</span>`
@@ -12,7 +16,7 @@ function addingProductsTemplate(products, productsStructure, productsWrapper) {
             const totalDiscount = (price * discountPercent) / 100
             const total = price - totalDiscount
 
-            productsWrapper.insertAdjacentHTML("beforeend", `<div class="col col-lg-3 product-main-box product-main-box__shop product-main-box__shop--show "><div class="product-box"  id="discount"><div   
+            element.innerHTML = `<div class="product-box"  id="discount"><div   
      class="img-box-overlay"><img class="product__img product__img-col" src="${product.img}" alt="product img"><div class="product-overlay">
      <div class="box-add-btn-cart"><a href="../Pages/product.html?id= ${product.id} " class="box-add-btn-cart__text">Details Product</a></div> 
      <div class="product-overlay__options"> 
@@ -24,11 +28,14 @@ function addingProductsTemplate(products, productsStructure, productsWrapper) {
      <div class="option-overlay"><i class="fa-regular fa-heart icon"></i>
      <span class="option-overlay__text">Like</span></div></div></div> ${product.discount ? discountTemplate : product.newProduct ? newTemplate : ""}</div><div class="product-Introductions"><h3 class="product-name">  ${product.productName}  </h3><p class="product-Introduction"> ${product.productIntroduction}   </p><div class="product-price-box"> 
     <span class="product-price"> Rp  ${total.toLocaleString("en")}   ${product.discount ? discountPrice : ""}
-    </div> </div></div></div>  `)
+    </div> </div></div>`
+            fragment.append(element)
         })
     } else {
         productsWrapper.innerHTML = '';
         products.forEach(function (product) {
+            const element = document.createElement("div")
+            element.className = `col col-lg-12 product-main-box product-main-box__shop product-main-box__shop--show `
             const discountTemplate = `<div class="Discount-product"><span class="Discount-product__number">${product.discountPercent}%</span></div>`
             const newTemplate = ` <div class="Discount-product Discount-product--green "><span class="Discount-product__number">NEW</span></div>`
             const discountPrice = `</span > <span class="product-discountPrice">Rp ${product.price}</span>`
@@ -38,7 +45,7 @@ function addingProductsTemplate(products, productsStructure, productsWrapper) {
             const totalDiscount = (price * discountPercent) / 100
             const total = price - totalDiscount
 
-            productsWrapper.insertAdjacentHTML("beforeend", `<div class="col col-lg-12 product-main-box product-main-box__shop product-main-box__shop--show "><div class="product-box product-box-col"  id="discount"><div   
+            element.innerHTML = `<div class="product-box product-box-col"  id="discount"><div   
      class="img-box-overlay img-box-overlay-col "><img class="product__img" src="${product.img}" alt="product img"><div class="product-overlay">
      <div class="box-add-btn-cart"><a href="../Pages/product.html?id= ${product.id} " class="box-add-btn-cart__text">Details Product</a></div> 
      <div class="product-overlay__options"> 
@@ -50,10 +57,11 @@ function addingProductsTemplate(products, productsStructure, productsWrapper) {
      <div class="option-overlay"><i class="fa-regular fa-heart icon"></i>
      <span class="option-overlay__text">Like</span></div></div></div> ${product.discount ? discountTemplate : product.newProduct ? newTemplate : ""}</div><div class="product-Introductions product-Introductions-col "><h3 class="product-name product-name-col ">  ${product.productName}  </h3><p class="product-Introduction product-Introduction-col"> ${product.productIntroduction}   </p><div class="product-price-box"> 
     <span class="product-price"> Rp  ${total.toLocaleString("en")}   ${product.discount ? discountPrice : ""}
-    </div> </div></div></div>  `)
+    </div> </div></div>`
+            fragment.append(element)
         })
-
     }
+    productsWrapper.append(fragment)
 }
 
 

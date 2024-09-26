17df16a6ee088530d3bf52819544aadc93272053
arwinGhaderi <arwinghaderi81@gmail.com>
2024-09-26 13:40:44 +0330

    bulid : Adding the slider automatic playback feature

--- js/slaider.js
+++ js/slaider.js
@@ -1,7 +1,14 @@
+const progressCircle = document.querySelector(".autoplay-progress svg");
+const progressContent = document.querySelector(".autoplay-progress span");
 const swiper = new Swiper(".mySwiper", {
     slidesPerView: 3,
     grabCursor: true,
-    spaceBetween: 50,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 3000,
+        slidesPerGroupAuto: true,
+        disableOnInteraction: false
+    },
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
@@ -9,12 +16,16 @@ const swiper = new Swiper(".mySwiper", {
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
+    }, on: {
+        autoplayTimeLeft(s, time, progress) {
+            progressCircle.style.setProperty("--progress", 1 - progress);
+            progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+        }
     },
     breakpoints: {
         // when window width is >= 320px
         320: {
             slidesPerView: 1,
-
         },
         // when window width is >= 480px
         480: {

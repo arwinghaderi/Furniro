26b49a4736a9988e1ffcf95df3fb11a47eecaba2
arwinGhaderi <arwinghaderi81@gmail.com>
2024-09-27 13:45:56 +0330

    refactor : scoring template refactor

--- js/singleProduct.js
+++ js/singleProduct.js
@@ -60,8 +60,16 @@ function setSinglePageProductDom(ProductSelectionByUser) {
         // #ffc700; "
 
 
-        containerStarCustomerReview.insertAdjacentHTML("afterbegin", ' <div class="detailes-produc-Specifications__score"><i class="fa-regular detailes-product-Specifications__star fa-star"></i><svg data-star="2" class="detailes-product-Specifications__star" width="25" height="25"viewBox="0 0 18 18" fill="white" fill="white" stroke="#FFC700"><path class="detailes-product-Specifications__star-path"d="M9 1.11803L11.5528 6.22361L11.6725 6.46295L11.938 6.49614L16.993 7.12801L13.5068 11.0372L13.347 11.2165L13.387 11.4533L14.343 17.1125L9.22361 14.5528L9 14.441L8.77639 14.5528L3.65861 17.1117L4.62289 11.454L4.66338 11.2164L4.50277 11.0368L1.00842 7.12784L6.06202 6.49614L6.32754 6.46295L6.44721 6.22361L9 1.11803Z" /></svg><svg data-star="3" class="detailes-product-Specifications__star" width="25" height="25"viewBox="0 0 18 18" fill="white" fill="white" stroke="#FFC700"><path class="detailes-product-Specifications__star-path"d="M9 1.11803L11.5528 6.22361L11.6725 6.46295L11.938 6.49614L16.993 7.12801L13.5068 11.0372L13.347 11.2165L13.387 11.4533L14.343 17.1125L9.22361 14.5528L9 14.441L8.77639 14.5528L3.65861 17.1117L4.62289 11.454L4.66338 11.2164L4.50277 11.0368L1.00842 7.12784L6.06202 6.49614L6.32754 6.46295L6.44721 6.22361L9 1.11803Z" /></svg><svg data-star="4" class="detailes-product-Specifications__star" width="25" height="25"viewBox="0 0 18 18" fill="white" fill="white" stroke="#FFC700"><path class="detailes-product-Specifications__star-path"d="M9 1.11803L11.5528 6.22361L11.6725 6.46295L11.938 6.49614L16.993 7.12801L13.5068 11.0372L13.347 11.2165L13.387 11.4533L14.343 17.1125L9.22361 14.5528L9 14.441L8.77639 14.5528L3.65861 17.1117L4.62289 11.454L4.66338 11.2164L4.50277 11.0368L1.00842 7.12784L6.06202 6.49614L6.32754 6.46295L6.44721 6.22361L9 1.11803Z" /></svg>  <svg data-star="5" class="detailes-product-Specifications__star" width="25" height="25"viewBox = "0 0 18 18" fill = "white" fill = "white" stroke = "#FFC700" ><path class="detailes-product-Specifications__star-path"' +
-            'd="M9 1.11803L11.5528 6.22361L11.6725 6.46295L11.938 6.49614L16.993 7.12801L13.5068 11.0372L13.347 11.2165L13.387 11.4533L14.343 17.1125L9.22361 14.5528L9 14.441L8.77639 14.5528L3.65861 17.1117L4.62289 11.454L4.66338 11.2164L4.50277 11.0368L1.00842 7.12784L6.06202 6.49614L6.32754 6.46295L6.44721 6.22361L9 1.11803Z" /></svg > </div ><div class="detailes-produc-Specifications__line line"></div><p class="detailes-produc-Specifications__status">0 Customer Review</p>')
+        containerStarCustomerReview.insertAdjacentHTML("afterbegin", `
+    <div class="detailes-produc-Specifications__score">
+      <button class="btn-icon"><i class="fa-regular detailes-product-Specifications__star fa-star"></i></button>
+      <button class="btn-icon"><i class="fa-regular detailes-product-Specifications__star fa-star"></i></button>
+      <button class="btn-icon"><i class="fa-regular detailes-product-Specifications__star fa-star"></i></button>
+      <button class="btn-icon"><i class="fa-regular detailes-product-Specifications__star fa-star"></i></button>
+      <button class="btn-icon"><i class="fa-regular detailes-product-Specifications__star fa-star"></i></button>
+    </div>
+    <div class="detailes-produc-Specifications__line line"></div>
+    <p class="detailes-produc-Specifications__status">0 Customer Review</p>`)
 
 
         containerDescriptionProduct.insertAdjacentHTML("afterbegin", '<p class="detailes-product__description">' + ProductSelectionByUser.description + '</p > ')
@@ -72,7 +80,7 @@ function setSinglePageProductDom(ProductSelectionByUser) {
             'd = "M0.833252 2.36501C0.833252 1.95879 0.994624 1.5692 1.28187 1.28196C1.56911 0.994717 1.9587 0.833346 2.36492 0.833346H17.6333C17.8346 0.833017 18.034 0.872398 18.22 0.949234C18.4061 1.02607 18.5752 1.13885 18.7176 1.28113C18.8601 1.4234 18.973 1.59237 19.0501 1.77835C19.1271 1.96434 19.1667 2.1637 19.1666 2.36501V17.6333C19.1668 17.8347 19.1273 18.0341 19.0504 18.2202C18.9735 18.4063 18.8606 18.5754 18.7183 18.7178C18.5759 18.8602 18.4069 18.9731 18.2209 19.0502C18.0348 19.1272 17.8354 19.1668 17.6341 19.1667H2.36492C2.16371 19.1667 1.96447 19.127 1.77858 19.05C1.5927 18.973 1.42381 18.8601 1.28157 18.7178C1.13933 18.5755 1.02653 18.4065 0.949604 18.2206C0.87268 18.0347 0.833143 17.8354 0.833252 17.6342V2.36501ZM8.08992 7.82335H10.5724V9.07001C10.9308 8.35335 11.8474 7.70835 13.2249 7.70835C15.8658 7.70835 16.4916 9.13585 16.4916 11.755V16.6067H13.8191V12.3517C13.8191 10.86 13.4608 10.0183 12.5508 10.0183C11.2883 10.0183 10.7633 10.9258 10.7633 12.3517V16.6067H8.08992V7.82335ZM3.50659 16.4925H6.17992V7.70835H3.50659V16.4917V16.4925ZM6.56242 4.84335C6.56746 5.07224 6.52673 5.29983 6.44262 5.51277C6.35851 5.72571 6.23271 5.91971 6.07261 6.08337C5.91251 6.24704 5.72133 6.37707 5.5103 6.46585C5.29926 6.55463 5.07262 6.60036 4.84367 6.60036C4.61472 6.60036 4.38808 6.55463 4.17704 6.46585C3.966 6.37707 3.77483 6.24704 3.61473 6.08337C3.45463 5.91971 3.32883 5.72571 3.24472 5.51277C3.16061 5.29983 3.11988 5.07224 3.12492 4.84335C3.13481 4.39406 3.32024 3.9665 3.64149 3.65225C3.96274 3.338 4.39427 3.16203 4.84367 3.16203C5.29307 3.16203 5.7246 3.338 6.04585 3.65225C6.3671 3.9665 6.55253 4.39406 6.56242 4.84335Z"fill="black" /></svg><svg class="Supplementary-specifications__icon" width="23" height="23"viewBox="0 0 23 23" fill="none">' +
             '<path d="M11.5 0.5625C5.45996 0.5625 0.5625 5.45996 0.5625 11.5C0.5625 17.54 5.45996 22.4375 11.5 22.4375C17.54 22.4375 22.4375 17.54 22.4375 11.5C22.4375 5.45996 17.54 0.5625 11.5 0.5625ZM16.7563 8.80713C16.7637 8.92187 16.7637 9.0415 16.7637 9.15869C16.7637 12.7427 14.0342 16.8711 9.04639 16.8711C7.5083 16.8711 6.08252 16.4243 4.88135 15.6553C5.10107 15.6797 5.31104 15.6895 5.53565 15.6895C6.80518 15.6895 7.97217 15.2598 8.90234 14.5322C7.71094 14.5078 6.70996 13.7266 6.36816 12.6523C6.78564 12.7134 7.16162 12.7134 7.59131 12.6035C6.97785 12.4789 6.42645 12.1457 6.0308 11.6606C5.63515 11.1755 5.41965 10.5684 5.4209 9.94238V9.9082C5.77979 10.1108 6.20215 10.2354 6.64404 10.2524C6.27256 10.0049 5.96791 9.66946 5.75711 9.27595C5.5463 8.88244 5.43585 8.443 5.43555 7.99658C5.43555 7.49121 5.56738 7.02979 5.8042 6.62939C6.48511 7.46762 7.33479 8.15318 8.29801 8.64152C9.26123 9.12986 10.3164 9.41004 11.395 9.46387C11.0117 7.62061 12.3887 6.12891 14.0439 6.12891C14.8252 6.12891 15.5283 6.45605 16.0239 6.9834C16.6367 6.86865 17.2227 6.63916 17.7451 6.33154C17.5425 6.95898 17.1177 7.48877 16.5537 7.82324C17.1006 7.76465 17.6279 7.61328 18.1162 7.40088C17.7476 7.94287 17.2861 8.42383 16.7563 8.80713Z"fill="black" /></svg></div >')
 
-        let iconStar = document.querySelectorAll(".detailes-product-Specifications__star")
+        let iconStar = document.querySelectorAll(".btn-icon")
         let statusScore = document.querySelector(".detailes-produc-Specifications__status")
         let btnSizeBox = document.querySelectorAll(".detailes-product__size-btn-box")
         let btnColor = document.querySelectorAll(".detailes-product__Color-btn-box ")
@@ -159,7 +167,6 @@ function setLocalStorgeProductArrayCart(cartArray) {
 
 function getLocalStorgeProductArrayCart() {
     let getCartArray = JSON.parse(localStorage.getItem("ProductArrayCart"))
-    console.log(getCartArray);
     if (getCartArray) {
         cartArray = getCartArray
         valuecountProductIcon = cartArray.length - 1
@@ -180,10 +187,12 @@ function setScoreProduct(iconStar, statusScore) {
             let curentStar = i + 1
             iconStar.forEach(function (icons, j) {
                 if (curentStar >= j + 1) {
-                    icons.classList.add("detailes-produc-Specifications__star--active")
+                    icons.firstChild.classList.add("fa-solid")
+                    icons.firstChild.classList.remove("fa-regular")
                     statusScore.innerHTML = curentStar + "  of" + ' 5 Customer Review';
                 } else {
-                    icons.classList.remove("detailes-produc-Specifications__star--active")
+                    icons.firstChild.classList.add("fa-regular")
+                    icons.firstChild.classList.remove("fa-solid")
                 }
             })
         })

bc19fab310875b8857d6f82216ac79110d8a4728
arwinGhaderi <arwinghaderi81@gmail.com>
2024-09-27 16:18:53 +0330

    refactor : Changing the template type to literal

--- js/singleProduct.js
+++ js/singleProduct.js
@@ -7,9 +7,8 @@ let $ = document
 // **elment html Single Page Product 
 
 
-let imgMain = document.querySelector(".detailes-product-img-main__box")
-let detailesProducSpecifications = document.querySelector(".detailes-produc-Specifications")
-let containerStarCustomerReview = document.querySelector(".detailes-produc-Specifications__customer-review-box")
+
+
 let containerDescriptionProduct = document.querySelector(".detailes-product__description-box")
 let containerBtnSiza = document.querySelector(".detailes-product__size-box")
 let containerColorBtn = document.querySelector(".detailes-product__color-box")
@@ -31,36 +30,33 @@ let ProductSelectionByUser = products.find(function (params) {
 
 const AddingPagePathDom = () => {
     const routeProduct = document.querySelector(".route-product")
+
     routeProduct.insertAdjacentHTML("afterbegin", `<div class="container"><div class= "route-product__waraper"><p class="route-product__path-name">Home</p><i class="fa-solid fa-angle-right fa-xs"></i><p class="route-product__path-name">Shop</p><i class="fa-solid fa-angle-right fa-xs"></i><div class="route-product__line-col line"></div><p class="route-product__product-name"> ${ProductSelectionByUser.productName}</p></></div >`)
 }
 
 const addingAllProductPhotos = () => {
+    const imgMain = document.querySelector(".detailes-product-img-main__box")
     const imgSecoundContainer = document.querySelector(".detailes-product-img-secound")
+
+    imgMain.insertAdjacentHTML("afterbegin", `<img class="detailes-product-img-main___img" src="${ProductSelectionByUser.imgSecoundMain}" alt="main-product">`)
+
     imgSecoundContainer.insertAdjacentHTML("afterbegin", ` <div class="detailes-product-img-secound__box  detailes-product-img-secound__box--active "><img class="detailes-product-img-secound__img" src =" ${ProductSelectionByUser.imgSecoundMain}" alt = "main-product" ></div ><div class="detailes-product-img-secound__box"><img class="detailes-product-img-secound__img" src="${ProductSelectionByUser.imgSecound1}"alt="detailes-product"></div><div class="detailes-product-img-secound__box"><img class="detailes-product-img-secound__img" src="${ProductSelectionByUser.imgSecound2} " alt="detailes-product"></div><div class="detailes-product-img-secound__box"><img class="detailes-product-img-secound__img" src="  ${ProductSelectionByUser.imgSecound3}"alt="detailes-product"></div><div class="detailes-product-img-secound__box"><img class="detailes-product-img-secound__img" src="  ${ProductSelectionByUser.imgSecound4} " alt="detailes-product"></div>`)
 }
 
 const addingProductPriceSectionTemplate = () => {
+    const detailesProducSpecifications = document.querySelector(".detailes-produc-Specifications")
     let price = +ProductSelectionByUser.price
     let discountPercent = +ProductSelectionByUser.discountPercent
     let totalDiscount = (price * discountPercent) / 100
     let total = price - totalDiscount
+
     ProductSelectionByUser.discount === true ? detailesProducSpecifications.insertAdjacentHTML("afterbegin", `<h3 class="detailes-produc-Specifications__title"> ${ProductSelectionByUser.productIntroduction}  </h3><h5 class= "detailes-produc-Specifications__price-discount" > Rp ${ProductSelectionByUser.price.toLocaleString("en")} </h5 ><h5 class="detailes-produc-Specifications__price">Rp  ${total.toLocaleString("en")}  </h5>`) : detailesProducSpecifications.insertAdjacentHTML("afterbegin", `<h3 class="detailes-produc-Specifications__title">${ProductSelectionByUser.productIntroduction}  </h3><h5 class= "detailes-produc-Specifications__price" > Rp  ${ProductSelectionByUser.price.toLocaleString("en")}  </h5>`)
 }
 
+const addingScoringTemplate = () => {
+    let containerStarCustomerReview = document.querySelector(".detailes-produc-Specifications__customer-review-box")
 
-function setSinglePageProductDom(ProductSelectionByUser) {
-    if (ProductSelectionByUser) {
-        AddingPagePathDom()
-        addingAllProductPhotos()
-        addingProductPriceSectionTemplate()
-        imgMain.insertAdjacentHTML("afterbegin", `<img class="detailes-product-img-main___img" src="${ProductSelectionByUser.imgSecoundMain}" alt="main-product">`)
-
-
-
-        // #ffc700; "
-
-
-        containerStarCustomerReview.insertAdjacentHTML("afterbegin", `
+    containerStarCustomerReview.insertAdjacentHTML("afterbegin", `
     <div class="detailes-produc-Specifications__score">
       <button class="btn-icon"><i class="fa-regular detailes-product-Specifications__star fa-star"></i></button>
       <button class="btn-icon"><i class="fa-regular detailes-product-Specifications__star fa-star"></i></button>
@@ -70,15 +66,40 @@ function setSinglePageProductDom(ProductSelectionByUser) {
     </div>
     <div class="detailes-produc-Specifications__line line"></div>
     <p class="detailes-produc-Specifications__status">0 Customer Review</p>`)
+}
 
 
-        containerDescriptionProduct.insertAdjacentHTML("afterbegin", '<p class="detailes-product__description">' + ProductSelectionByUser.description + '</p > ')
-        containerBtnSiza.insertAdjacentHTML("afterbegin", ' <span class="detailes-product__size__name">Size</span><div class= "detailes-product__size-btn-container" ><div class="detailes-product__size-btn-box  detailes-product__size-btn-box--active"><butuon class="detailes-product__size-btn  detailes-product__size-btn--active">L</butuon></div><div class="detailes-product__size-btn-box"><butuon class="detailes-product__size-btn">XL</butuon></div><div class="detailes-product__size-btn-box"><butuon class="detailes-product__size-btn">XS</butuon></div></div> ')
-        containerColorBtn.insertAdjacentHTML("afterbegin", '  <span class="detailes-product__Color__name">Color</span><div class="detailes-product__Color-btn-container"> <div data-color="' + ProductSelectionByUser.color1 + '" class="detailes-product__Color-btn-box  detailes-product__size-btn--' + ProductSelectionByUser.color1 + ' detailes-product__Color-btn-box--active"> </div> <div data-color="' + ProductSelectionByUser.color2 + '"  class="detailes-product__Color-btn-box detailes-product__size-btn--' + ProductSelectionByUser.color2 + '"> </div><div data-color="' + ProductSelectionByUser.color3 + '" class="detailes-product__Color-btn-box  detailes-product__size-btn--' + ProductSelectionByUser.color3 + '"></div><div data-color="' + ProductSelectionByUser.color4 + '"<div class="detailes-product__Color-btn-box  detailes-product__size-btn--' + ProductSelectionByUser.color4 + '"></div>  </div >')
-        containerBtnProduct.insertAdjacentHTML("beforeend", '<div  class="detailes-product-btn__box-cart"><button class="detailes-product-btn__cart detailes-product-btn__cart--add-to-cart">Add To Cart</button></div><div class="detailes-product-btn__box-Compare"> <button class="detailes-product-btn__Compare">+Compare</button></div>')
-        containerInformation.insertAdjacentHTML("afterbegin", '<span class="Supplementary-specifications__value">SS00' + ProductSelectionByUser.id + '</span><span class="Supplementary-specifications__value">' + ProductSelectionByUser.type + '</span ><span class="Supplementary-specifications__value">Sofa, Chair, Home, Shop</span>   <div class="Supplementary-specifications__box-icon"><svg class= "Supplementary-specifications__icon" width = "20" height = "20"viewBox = "0 0 20 20" fill = "none" ><path fill-rule="evenodd" clip-rule="evenodd"d="M0 10.0558C0 15.0275 3.61083 19.1617 8.33333 20V12.7775H5.83333V10H8.33333V7.7775C8.33333 5.2775 9.94417 3.88917 12.2225 3.88917C12.9442 3.88917 13.7225 4 14.4442 4.11083V6.66667H13.1667C11.9442 6.66667 11.6667 7.2775 11.6667 8.05583V10H14.3333L13.8892 12.7775H11.6667V20C16.3892 19.1617 20 15.0283 20 10.0558C20 4.525 15.5 0 10 0C4.5 0 0 4.525 0 10.0558Z"fill="black" /></svg ><svg class="Supplementary-specifications__icon" width="20" height="20"viewBox="0 0 20 20" fill="none"><path fill-rule="evenodd" clip-rule="evenodd"' +
-            'd = "M0.833252 2.36501C0.833252 1.95879 0.994624 1.5692 1.28187 1.28196C1.56911 0.994717 1.9587 0.833346 2.36492 0.833346H17.6333C17.8346 0.833017 18.034 0.872398 18.22 0.949234C18.4061 1.02607 18.5752 1.13885 18.7176 1.28113C18.8601 1.4234 18.973 1.59237 19.0501 1.77835C19.1271 1.96434 19.1667 2.1637 19.1666 2.36501V17.6333C19.1668 17.8347 19.1273 18.0341 19.0504 18.2202C18.9735 18.4063 18.8606 18.5754 18.7183 18.7178C18.5759 18.8602 18.4069 18.9731 18.2209 19.0502C18.0348 19.1272 17.8354 19.1668 17.6341 19.1667H2.36492C2.16371 19.1667 1.96447 19.127 1.77858 19.05C1.5927 18.973 1.42381 18.8601 1.28157 18.7178C1.13933 18.5755 1.02653 18.4065 0.949604 18.2206C0.87268 18.0347 0.833143 17.8354 0.833252 17.6342V2.36501ZM8.08992 7.82335H10.5724V9.07001C10.9308 8.35335 11.8474 7.70835 13.2249 7.70835C15.8658 7.70835 16.4916 9.13585 16.4916 11.755V16.6067H13.8191V12.3517C13.8191 10.86 13.4608 10.0183 12.5508 10.0183C11.2883 10.0183 10.7633 10.9258 10.7633 12.3517V16.6067H8.08992V7.82335ZM3.50659 16.4925H6.17992V7.70835H3.50659V16.4917V16.4925ZM6.56242 4.84335C6.56746 5.07224 6.52673 5.29983 6.44262 5.51277C6.35851 5.72571 6.23271 5.91971 6.07261 6.08337C5.91251 6.24704 5.72133 6.37707 5.5103 6.46585C5.29926 6.55463 5.07262 6.60036 4.84367 6.60036C4.61472 6.60036 4.38808 6.55463 4.17704 6.46585C3.966 6.37707 3.77483 6.24704 3.61473 6.08337C3.45463 5.91971 3.32883 5.72571 3.24472 5.51277C3.16061 5.29983 3.11988 5.07224 3.12492 4.84335C3.13481 4.39406 3.32024 3.9665 3.64149 3.65225C3.96274 3.338 4.39427 3.16203 4.84367 3.16203C5.29307 3.16203 5.7246 3.338 6.04585 3.65225C6.3671 3.9665 6.55253 4.39406 6.56242 4.84335Z"fill="black" /></svg><svg class="Supplementary-specifications__icon" width="23" height="23"viewBox="0 0 23 23" fill="none">' +
-            '<path d="M11.5 0.5625C5.45996 0.5625 0.5625 5.45996 0.5625 11.5C0.5625 17.54 5.45996 22.4375 11.5 22.4375C17.54 22.4375 22.4375 17.54 22.4375 11.5C22.4375 5.45996 17.54 0.5625 11.5 0.5625ZM16.7563 8.80713C16.7637 8.92187 16.7637 9.0415 16.7637 9.15869C16.7637 12.7427 14.0342 16.8711 9.04639 16.8711C7.5083 16.8711 6.08252 16.4243 4.88135 15.6553C5.10107 15.6797 5.31104 15.6895 5.53565 15.6895C6.80518 15.6895 7.97217 15.2598 8.90234 14.5322C7.71094 14.5078 6.70996 13.7266 6.36816 12.6523C6.78564 12.7134 7.16162 12.7134 7.59131 12.6035C6.97785 12.4789 6.42645 12.1457 6.0308 11.6606C5.63515 11.1755 5.41965 10.5684 5.4209 9.94238V9.9082C5.77979 10.1108 6.20215 10.2354 6.64404 10.2524C6.27256 10.0049 5.96791 9.66946 5.75711 9.27595C5.5463 8.88244 5.43585 8.443 5.43555 7.99658C5.43555 7.49121 5.56738 7.02979 5.8042 6.62939C6.48511 7.46762 7.33479 8.15318 8.29801 8.64152C9.26123 9.12986 10.3164 9.41004 11.395 9.46387C11.0117 7.62061 12.3887 6.12891 14.0439 6.12891C14.8252 6.12891 15.5283 6.45605 16.0239 6.9834C16.6367 6.86865 17.2227 6.63916 17.7451 6.33154C17.5425 6.95898 17.1177 7.48877 16.5537 7.82324C17.1006 7.76465 17.6279 7.61328 18.1162 7.40088C17.7476 7.94287 17.2861 8.42383 16.7563 8.80713Z"fill="black" /></svg></div >')
+function setSinglePageProductDom(ProductSelectionByUser) {
+    if (ProductSelectionByUser) {
+        AddingPagePathDom()
+        addingAllProductPhotos()
+        addingProductPriceSectionTemplate()
+        addingScoringTemplate()
+
+        containerDescriptionProduct.insertAdjacentHTML("afterbegin", `<p class="detailes-product__description">${ProductSelectionByUser.description}  </p > `)
+
+        containerBtnSiza.insertAdjacentHTML("afterbegin",
+            ` <span class="detailes-product__size__name">Size</span><div class= "detailes-product__size-btn-container" ><div class="detailes-product__size-btn-box  detailes-product__size-btn-box--active"><butuon class="detailes-product__size-btn  detailes-product__size-btn--active">L</butuon></div><div class="detailes-product__size-btn-box"><butuon class="detailes-product__size-btn">XL</butuon></div><div class="detailes-product__size-btn-box"><butuon class="detailes-product__size-btn">XS</butuon></div></div> `)
+
+        containerColorBtn.insertAdjacentHTML("afterbegin",
+            `<span class="detailes-product__Color__name">Color</span><div class="detailes-product__Color-btn-container"> <div data-color="${ProductSelectionByUser.color1}" class="detailes-product__Color-btn-box  detailes-product__size-btn--${ProductSelectionByUser.color1} detailes-product__Color-btn-box--active"> </div> <div data-color="${ProductSelectionByUser.color2}"  class="detailes-product__Color-btn-box detailes-product__size-btn--${ProductSelectionByUser.color2}"> </div><div data-color="${ProductSelectionByUser.color3}" class="detailes-product__Color-btn-box  detailes-product__size-btn--${ProductSelectionByUser.color3}"></div><div data-color="${ProductSelectionByUser.color4}"<div class="detailes-product__Color-btn-box  detailes-product__size-btn--${ProductSelectionByUser.color4}"></div></div>`)
+
+
+        containerBtnProduct.insertAdjacentHTML("beforeend", `<div  class="detailes-product-btn__box-cart"><button class="detailes-product-btn__cart detailes-product-btn__cart--add-to-cart">Add To Cart</button></div><div class="detailes-product-btn__box-Compare"> <button class="detailes-product-btn__Compare">+Compare</button></div>`)
+
+        containerInformation.insertAdjacentHTML("afterbegin",
+            `<span class="Supplementary-specifications__value">SS00${ProductSelectionByUser.id}   </span><span class="Supplementary-specifications__value">${ProductSelectionByUser.type}</span ><span class="Supplementary-specifications__value">Sofa, Chair, Home, Shop</span> 
+            <div class="Supplementary-specifications__box-icon">
+             <div class="box-Icon-social">
+             <i class="fa-brands fa-facebook-f Icon-social"></i>
+            </div>
+             <div class="box-Icon-social  box-Icon-social--linkdin">
+             <i class="fa-brands fa-linkedin-in Icon-social"></i>
+            </div>
+             <div class="box-Icon-social box-Icon-social--twitter">
+             <i class="fa-brands fa-twitter Icon-social"></i>
+            </div>
+            </div >`)
 
         let iconStar = document.querySelectorAll(".btn-icon")
         let statusScore = document.querySelector(".detailes-produc-Specifications__status")

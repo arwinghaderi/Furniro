b9a9a210798bb46b6e7961c9bdaab88438a1b422
arwinGhaderi <arwinghaderi81@gmail.com>
2024-10-01 12:11:45 +0330

    feat :Adding products to the cart page

--- js/cartPage.js
+++ js/cartPage.js
@@ -1,9 +1,13 @@
-// import { cartArray } from "./singleProduct.js"
-// console.log(cartArray);
+import { getFromLocalStorage } from "../js/func/utils.js"
+let $ = document
+let productsCart = getFromLocalStorage("cartShopProducts")
 
 
+let keeperProductPageCart = document.querySelector(".cart-shop-section__shop")
+productsCart.forEach(product => {
+    keeperProductPageCart.insertAdjacentHTML("beforeend", ` <div class="cart-shop-section__details-product"><div div class= "cart-shop-section__box-img"><img class="cart-shop-section__img" src="${product.imgSecoundMain}  " alt="product Image"></div><span class="cart-shop-section__product-name"> ${product.productName} </span><span class="cart-shop-section__product-price"> Rs ${product.discount ? total.toLocaleString("en") : product.price.toLocaleString("en")} </span><div class="cart-shop-section__product-Quantity-box"><input class="cart-shop-section__product-Quantity" type="number" value="${product.count}"></div><span class="cart-shop-section__product-Subtotal"> Rs. ${product.discount ? total.toLocaleString("en") : product.price.toLocaleString("en")}</span><svg class="cart-shop-section__icon-delete" width="22" height="22" viewBox="0 0 22 22" fill="none" >
+     <path d="M20.625 4H17.125V1.8125C17.125 0.847266 16.3402 0.0625 15.375 0.0625H6.625C5.65977 0.0625 4.875 0.847266 4.875 1.8125V4H1.375C0.891016 4 0.5 4.39102 0.5 4.875V5.75C0.5 5.87031 0.598437 5.96875 0.71875 5.96875H2.37031L3.0457 20.2695C3.08945 21.202 3.86055 21.9375 4.79297 21.9375H17.207C18.1422 21.9375 18.9105 21.2047 18.9543 20.2695L19.6297 5.96875H21.2812C21.4016 5.96875 21.5 5.87031 21.5 5.75V4.875C21.5 4.39102 21.109 4 20.625 4ZM15.1562 4H6.84375V2.03125H15.1562V4Z" fill="#B88E2F"/>
+     </svg></div > `)
+});
 
-//     //** */ templete page-Cart
-// let keeperProductPageCart = document.querySelector(".cart-shop-section__shop")
-//     keeperProductPageCart.insertAdjacentHTML("beforeend", ' <div class="cart-shop-section__details-product"><div div class= "cart-shop-section__box-img"><img class="cart-shop-section__img" src="' + product.imgSecoundMain + '" alt="product Image"></div><span class="cart-shop-section__product-name">' + product.productName + '</span><span class="cart-shop-section__product-price">' + product.price + '</span><div class="cart-shop-section__product-Quantity-box"><input class="cart-shop-section__product-Quantity" type="number" value="' + product.count + '"></div><span class="cart-shop-section__product-Subtotal">' + product.price + '</span><svg class="cart-shop-section__icon-delete" width="22" height="22" viewBox="0 0 22 22"fill="none"><pathd="M20.625 4H17.125V1.8125C17.125 0.847266 16.3402 0.0625 15.375 0.0625H6.625C5.65977 0.0625 4.875 0.847266 4.875 1.8125V4H1.375C0.891016 4 0.5 4.39102 0.5 4.875V5.75C0.5 5.87031 0.598437 5.96875 0.71875 5.96875H2.37031L3.0457 20.2695C3.08945 21.202 3.86055 21.9375 4.79297 21.9375H17.207C18.1422 21.9375 18.9105 21.2047 18.9543 20.2695L19.6297 5.96875H21.2812C21.4016 5.96875 21.5 5.87031 21.5 5.75V4.875C21.5 4.39102 21.109 4 20.625 4ZM15.1562 4H6.84375V2.03125H15.1562V4Z"fill="#B88E2F" /></svg></div > ')
 

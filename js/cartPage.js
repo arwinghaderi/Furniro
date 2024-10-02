1208c9b996fc6c7a56c682a049a8ad82f7b32f35
arwinGhaderi <arwinghaderi81@gmail.com>
2024-10-02 12:38:11 +0330

    feat : Adding function (removeProductByUser)

--- js/cartPage.js
+++ js/cartPage.js
@@ -1,19 +1,24 @@
 import { getFromLocalStorage, productDiscountCalculation, saveToLocalStorage } from "../js/func/utils.js"
 
 let $ = document
+let total, element
 let productsCart = getFromLocalStorage("cartShopProducts")
 const fragment = document.createDocumentFragment()
-let total, element
-const addingCartProductsTemplate = () => {
+
+const addingCartProductsTemplate = (productsCart) => {
     let keeperProductPageCart = document.querySelector(".cart-shop-section__shop")
-    if (productsCart) {
+    keeperProductPageCart.innerHTML = ""
+
+    if (productsCart && productsCart.length) {
+
         productsCart.forEach(product => {
             total = productDiscountCalculation(+product.price, +product.discountPercent)
             element = document.createElement("div")
 
-            element.innerHTML = `<div class="cart-shop-section__details-product"><div div class="cart-shop-section__box-img"><img class="cart-shop-section__img" src="${product.imgSecoundMain}  " alt="product Image"></div><span class="cart-shop-section__product-name"> ${product.productName} </span><span class="cart-shop-section__product-price"> Rs ${product.discount ? total.toLocaleString("en") : product.price.toLocaleString("en")} </span><div class="cart-shop-section__product-Quantity-box"><input min="1" max="10" oninput="selectionNumberProductsByUser(event,${product.id})" class="cart-shop-section__product-Quantity" type="number" value="${product.count}"></div><span class="cart-shop-section__product-Subtotal"> Rs. ${product.discount ? total.toLocaleString("en") : product.price.toLocaleString("en")}</span><i class="fa-solid fa-trash fa-sm icon-delte"></i></div > `
+            element.innerHTML = `<div class="cart-shop-section__details-product"><div div class="cart-shop-section__box-img"><img class="cart-shop-section__img" src="${product.imgSecoundMain}  " alt="product Image"></div><span class="cart-shop-section__product-name"> ${product.productName} </span><span class="cart-shop-section__product-price"> Rs ${product.discount ? total.toLocaleString("en") : product.price.toLocaleString("en")} </span><div class="cart-shop-section__product-Quantity-box"><input min="1" max="10" oninput="selectionNumberProductsByUser(event,${product.id})" class="cart-shop-section__product-Quantity" type="number" value="${product.count}"></div><span class="cart-shop-section__product-Subtotal"> Rs. ${product.discount ? total.toLocaleString("en") : product.price.toLocaleString("en")}</span><i onclick="removeProductByUser(${product.id})"  class="fa-solid fa-trash fa-sm icon-delte"></i></div > `
             fragment.append(element)
         });
+
     } else {
         keeperProductPageCart.insertAdjacentHTML("beforeend", `<div div class="alert alert-danger" > <p>(:هیچ محصولی در سبد خرید شما وجود نداد</p> </div > `)
     }
@@ -59,8 +64,22 @@ const calculationTotalCart = () => {
 }
 
 window.addEventListener("load", () => {
-    addingCartProductsTemplate()
+    addingCartProductsTemplate(productsCart)
     calculationTotalCart()
 })
 
+const removeProductByUser = (productId) => {
+    productsCart = productsCart.filter(product => {
+        return product.id !== productId
+    })
+    // cartProducts.length ? cartProducts.length : cartProducts.length + 1
+    // iconCountProducts.innerHTML--
+
+    saveToLocalStorage("countProductToCart", productsCart.length)
+    saveToLocalStorage("cartShopProducts", productsCart)
+    calculationTotalCart()
+    addingCartProductsTemplate(productsCart)
+}
+
 window.selectionNumberProductsByUser = selectionNumberProductsByUser;
+window.removeProductByUser = removeProductByUser

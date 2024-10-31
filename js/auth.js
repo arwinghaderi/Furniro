83d86f308d4c322ed09567444537ac09439e4903
arwinGhaderi <arwinghaderi81@gmail.com>
2024-10-31 18:39:20 +0330

    Adding JavaScript logic for authentication page

---
+++ js/auth.js
@@ -0,0 +1,33 @@
+const btnsAuth = document.querySelectorAll(".btn-Auth")
+const formSignIn = document.querySelector(".form-signIn")
+const signInTitle = document.querySelector(".signIn-title")
+const signupNow = document.querySelector(".signup-now")
+const btnSignUp = document.querySelector(".btn-signUp ")
+
+btnsAuth.forEach((btn) => {
+    btn.addEventListener("click", (event) => {
+        let dataset = event.target.dataset.auth
+
+        document.querySelector(".btn-signIn--active").classList.remove("btn-signIn--active")
+
+        event.target.classList.add("btn-signIn--active")
+        togglingAuthPage(dataset)
+    })
+})
+
+const togglingAuthPage = (authData) => {
+    if (authData === "signIn") {
+        formSignIn.style.marginLeft = "0"
+        signInTitle.style.marginLeft = "0"
+    } else {
+        formSignIn.style.marginLeft = `${-100}%`
+        signInTitle.style.marginLeft = `${-50}%`
+    }
+}
+
+signupNow.addEventListener("click", () => {
+    formSignIn.style.marginLeft = `${-100}%`
+    signInTitle.style.marginLeft = `${-50}%`
+    document.querySelector(".btn-signIn--active").classList.remove("btn-signIn--active")
+    btnSignUp.classList.add("btn-signIn--active")
+})

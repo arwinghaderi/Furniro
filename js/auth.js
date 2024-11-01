87ea685e9a0e4c34e64b5cf4ecba700b8a485f33
arwinGhaderi <arwinghaderi81@gmail.com>
2024-11-01 18:00:03 +0330

    feat : Adding show/hide password icon

--- js/auth.js
+++ js/auth.js
@@ -3,6 +3,7 @@ const formSignIn = document.querySelector(".form-signIn")
 const signInTitle = document.querySelector(".signIn-title")
 const signupNow = document.querySelector(".signup-now")
 const btnSignUp = document.querySelector(".btn-signUp ")
+const passwordBoxs = document.querySelectorAll('.togglePassword-box');
 
 btnsAuth.forEach((btn) => {
     btn.addEventListener("click", (event) => {
@@ -10,12 +11,13 @@ btnsAuth.forEach((btn) => {
 
         document.querySelector(".btn-signIn--active").classList.remove("btn-signIn--active")
 
-        event.target.classList.add("btn-signIn--active")
+        btn.classList.add("btn-signIn--active")
         togglingAuthPage(dataset)
     })
 })
 
 const togglingAuthPage = (authData) => {
+    console.log(authData);
     if (authData === "signIn") {
         formSignIn.style.marginLeft = "0"
         signInTitle.style.marginLeft = "0"
@@ -24,6 +26,23 @@ const togglingAuthPage = (authData) => {
         signInTitle.style.marginLeft = `${-50}%`
     }
 }
+
+passwordBoxs.forEach((passwordBox) => {
+    passwordBox.addEventListener("click", () => {
+        const input = passwordBox.previousElementSibling
+        const icon = passwordBox.firstElementChild
+        const type = input.getAttribute("type") === "password" ? "text" : "password"
+        input.setAttribute('type', type);
+
+        if (type === "password") {
+            icon.classList.add('fa-eye')
+            icon.classList.remove('fa-eye-slash')
+        } else {
+            icon.classList.add('fa-eye-slash')
+            icon.classList.remove('fa-eye')
+        }
+    })
+})
 
 signupNow.addEventListener("click", () => {
     formSignIn.style.marginLeft = `${-100}%`

6f6b7e6418f1bde27b82ca77a2c639a99587c99b
arwinGhaderi <arwinghaderi81@gmail.com>
2024-11-26 17:20:28 +0330

    build : Added input validation labels and modular validation method.

--- frontend/js/auth/auth.js
+++ frontend/js/auth/auth.js
@@ -4,6 +4,9 @@ const formSignIn = document.querySelector(".form-signIn")
 const signInTitle = document.querySelector(".signIn-title")
 const signupNow = document.querySelector(".signup-now")
 const btnSignUp = document.querySelector(".btn-signUp ")
+import { validation } from "../func/utils.js"
+
+
 
 
 
@@ -35,18 +38,15 @@ signupNow.addEventListener("click", () => {
     btnSignUp.classList.add("btn-signIn--active")
 })
 
-const signInEmail = document.querySelector(".signIn-Email")
-const signInPassword = document.querySelector(".signIn-password")
 const btnSignIn = document.querySelector(".btn-signIn-register")
 
 const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8}$/;
-
+const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 
 const gettingUserInformation = () => {
     const userInformation = {
-        email: signInEmail.value.trim(),
-        password: signInPassword.value.trim()
+        email: loginEmailInput.value.trim(),
+        password: loginPasswordInput.value.trim()
     }
 
     let isEmailValid = validateEmail(userInformation.email)
@@ -57,16 +57,54 @@ const gettingUserInformation = () => {
 
 
 
-
 const validateEmail = (email) => {
-    console.log(email);
     return emailRegex.test(email);
 }
 const validatePassword = (password) => {
-    console.log(password);
     return passwordRegex.test(password);
 }
 
+const inputs = [
+    {
+        element: document.querySelector(".signIn-Email"),
+        regex: emailRegex,
+        validationEntries: {
+            valid: document.querySelector(".Email-signin-Valid"),
+            invalid: document.querySelector(".Email-signin-invalid ")
+        }
+    },
+    {
+        element: document.querySelector(".signIn-password"),
+        regex: passwordRegex,
+        validationEntries: {
+            valid: document.querySelector(".password-validation-Valid"),
+            invalid: document.getElementById('passwordSigninInvalid')
+        }
+    },
+    {
+        element: document.querySelector(".signUp-Email"),
+        regex: emailRegex,
+        validationEntries: {
+            valid: document.querySelector(".Email-signUp-Valid"),
+            invalid: document.querySelector(".Email-signUp-InValid")
+        }
+    },
+    {
+        element: document.querySelector(".signUp-confirm"),
+        regex: passwordRegex,
+        validationEntries: {
+            valid: document.querySelector(".password-confrim-Valid"),
+            invalid: document.querySelector(".password-confrim-invalid")
+        }
+    },
+]
+
+inputs.forEach(input => {
+    input.element.addEventListener('input', (event) => {
+        const value = event.target.value.trim();
+        validation(value, input.regex, input.validationEntries);
+    });
+});
 
 btnSignIn.addEventListener("click", (event) => {
     event.preventDefault()

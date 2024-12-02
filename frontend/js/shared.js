6bead17e86625331272638252de576468f1eab1c
arwinGhaderi <arwinghaderi81@gmail.com>
2024-12-02 21:09:19 +0330

    build : Handling user login success state

--- frontend/js/shared.js
+++ frontend/js/shared.js
@@ -20,16 +20,53 @@ const loginSuccessfully = $.querySelector(".login-successfully")
 const navbarSuccessfullyRegisterText = $.querySelector(".navbar-successfully-Register-text")
 const navbarDontRegisterText = $.querySelector(".navbar-dont-Register-text")
 
-const isLogin = checkingLoginStatus()
-if (isLogin) {
-    getingUaerInformation().then((data) => {
-        const email = data.payload.email
-        const username = email.split('@')[0]
 
-        loginSuccessfully.style.display = "flex"
-        navbarSuccessfullyRegisterText.innerHTML = username
-    })
-} else {
-    navbarDontRegisterText.innerHTML = `sign In/sign Up`
-    dontLogin.style.display = "flex"
+const handleUserAuthentication = () => {
+    const isLogin = checkingLoginStatus()
+    if (isLogin) {
+        getingUaerInformation().then((data) => {
+            const email = data.payload.email
+            const username = email.split('@')[0]
+
+            loginSuccessfully.style.display = "flex"
+            dontLogin.style.display = "none"
+            navbarSuccessfullyRegisterText.innerHTML = username
+        })
+    } else {
+        navbarDontRegisterText.innerHTML = `sign In/sign Up`
+        dontLogin.style.display = "flex"
+        loginSuccessfully.style.display = "none"
+    }
+
 }
+
+loginSuccessfully.addEventListener("click", () => {
+    Swal.fire({
+        title: "Exit Account?",
+        text: "Click here to securely log out of your account.",
+        icon: "warning",
+        customClass: { popup: 'custom-swal2' },
+        showCancelButton: true,
+        confirmButtonColor: "#B88E2F",
+        cancelButtonColor: "red",
+        confirmButtonText: "Log Out"
+    }).then((result) => {
+        if (result.isConfirmed) {
+            localStorage.removeItem('Access-Token');
+            handleUserAuthentication()
+
+            Swal.fire({
+                title: "Logged Out Successfully",
+                text: "You have been logged out. Thank you for visiting. Have a great day!",
+                icon: "success",
+                customClass: { popup: 'custom-swal2' },
+                confirmButtonText: 'ok',
+                confirmButtonColor: "#B88E2F",
+            });
+        }
+    });
+})
+
+window.addEventListener("load", () => {
+    handleUserAuthentication()
+})

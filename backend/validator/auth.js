8ec4909bd3cfc868f69b6d49ca90ab8166d26f36
khodaeidev <khodaei.dev@gmail.com>
2024-12-05 23:08:53 +0330

    Dev Auth Validator (Register and login)

---
+++ backend/validator/auth.js
@@ -0,0 +1,42 @@
+const yup = require("yup");
+
+let emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+let passwordRegex =
+  /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>])[A-Za-z\d!@#$%^&*(),.?":{}|<>]{8,}$/;
+
+exports.registerValidator = yup.object({
+  fullname: yup
+    .string()
+    .min(5, "The name must have at least 5 characters")
+    .max(100, "The name must have a maximum of 100 characters")
+    .required(),
+  email: yup
+    .string()
+    .matches(emailRegex, "The email entered is not valid.")
+    .required(),
+  password: yup
+    .string()
+    .required()
+    .matches(
+      passwordRegex,
+      "The password must contain at least one capital letter, one number, one special character and at least 8 characters"
+    ),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref("password"), null], "The code of worship does not match")
+    .required(),
+});
+
+exports.loginValidator = yup.object({
+  email: yup
+    .string()
+    .matches(emailRegex, "The email entered is not valid.")
+    .required(),
+  password: yup
+    .string()
+    .required()
+    .matches(
+      passwordRegex,
+      "The password must contain at least one capital letter, one number, one special character and at least 8 characters"
+    ),
+});

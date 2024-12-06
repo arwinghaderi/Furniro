8017818f7f63c61d0ad5efc54bdf6db2d586d9c6
khodaeidev <khodaei.dev@gmail.com>
2024-12-06 09:50:20 +0330

    Dev refresh and Login routes

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,5 +1,9 @@
 const express = require("express");
-const { userLogin, userRegister } = require("./../controller/auth");
+const {
+  userLogin,
+  userRegister,
+  getNewAccessToken,
+} = require("./../controller/auth");
 const validator = require("./../middleware/validator");
 const { registerValidator } = require("../validator/auth");
 
@@ -7,5 +11,7 @@ const router = express.Router();
 
 router.route("/register").post(validator(registerValidator), userRegister);
 router.route("/login").post(userLogin);
+
+router.route("/refresh").post(getNewAccessToken);
 
 module.exports = router;

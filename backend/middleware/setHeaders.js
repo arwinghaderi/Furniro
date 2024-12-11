d7d360c33016d50fe4477a55c2f59cc2df594439
khodaeidev <khodaei.dev@gmail.com>
2024-12-11 13:05:37 +0330

    Debug cors policy Err

--- backend/middleware/setHeaders.js
+++ backend/middleware/setHeaders.js
@@ -1,6 +1,11 @@
 exports.setHeaders = (req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, PUT, POST, DELETE, OPTIONS"
+  );
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+
   next();
 };

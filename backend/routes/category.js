77dfd77e341d7ea955f52b6aed03987dd3336ce5
khodaeidev <khodaei.dev@gmail.com>
2024-12-13 11:51:06 +0330

    Dev Remove Category logic

--- backend/routes/category.js
+++ backend/routes/category.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const { auth } = require("../middleware/auth");
 const { isAdmin } = require("../middleware/isAdmin");
-const { createCategory, getAllCategories } = require("../controller/category");
+const {
+  createCategory,
+  getAllCategories,
+  removeCategory,
+} = require("../controller/category");
 const validator = require("../middleware/validator");
 const { createCategoryValidator } = require("../validator/category");
 
@@ -11,5 +15,7 @@ router
   .route("/")
   .post(auth, isAdmin, validator(createCategoryValidator), createCategory)
   .get(getAllCategories);
+
+router.route("/:categoryId").delete(auth, isAdmin, removeCategory);
 
 module.exports = router;

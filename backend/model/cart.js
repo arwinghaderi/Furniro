825087237f9d5b1469ab114d1e73eb82936db241
khodaeidev <khodaei.dev@gmail.com>
2025-01-15 16:04:15 +0330

    Dev total userCart Price and Discount

--- backend/model/cart.js
+++ backend/model/cart.js
@@ -42,6 +42,24 @@ cartSchema.pre("save", function (next) {
   next();
 });
 
+cartSchema.virtual("totalPrice").get(function () {
+  let allPrice = 0;
+  let allDiscount = 0;
+
+  this.items.forEach((item) => {
+    const discountAmount =
+      (item.product.price * item.product.discountPercent) / 100;
+    const discountedPrice = item.product.price - discountAmount;
+
+    allDiscount += discountAmount * item.quantity;
+    allPrice += discountedPrice * item.quantity;
+  });
+
+  return {
+    allPrice,
+    allDiscount,
+  };
+});
 const model = mongoose.model("Cart", cartSchema);
 
 module.exports = model;

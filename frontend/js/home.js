103d2c44a063c20198664626051efebd89772e12
arwinGhaderi <arwinghaderi81@gmail.com>
2025-01-19 00:03:27 +0330

    feat: Adding Global Search Logic

--- frontend/js/home.js
+++ frontend/js/home.js
@@ -1,11 +1,17 @@
 import {
     getFromLocalStorage,
+    showAuthenticationRequiredAlert,
 } from "./func/utils.js"
+
+import { addingProductsTemplate } from "./func/shared.js"
+
+import { getToken } from "./func/utils.js"
 
 let productsWrapper = document.querySelector(".row-container")
 let productsStructure = 'row'
 let productsShowMore
 
+let productsHomeContainer = document.querySelector(".products-home-container")
 
 const addingProductsByUser = () => {
     productsShowMore = getFromLocalStorage("pageHomeProducts")
@@ -13,11 +19,37 @@ const addingProductsByUser = () => {
     // addingProductsTemplate(productsShowMore, productsStructure, productsWrapper)
 }
 
-window.addEventListener('load', () => {
-    // if (getFromLocalStorage("pageHomeProducts")) {
-    //     addingProductsByUser(products)
-    // } else {
-    //     addingProductsByUser(products)
-    // }
-    // getCountProductsCart()
+const fetchGetProducts = async () => {
+    let token = getToken();
+    try {
+        const url = new URL(`https://furniro-6x7f.onrender.com/`);
+
+        console.log(url);
+
+        const headers = {
+            'Content-Type': 'application/json',
+        };
+        if (token) {
+            headers['authorization'] = `Bearer ${token}`;
+        }
+
+        const response = await fetch(url, {
+            method: "GET",
+            headers: headers
+        });
+
+        if (!response.ok) {
+            let message = "An unexpected error occurred. Refresh the page.";
+            throw new Error(message);
+        }
+        const dataProducts = await response.json();
+        const lastFourProducts = dataProducts.data.products.slice(0, 4)
+        addingProductsTemplate(lastFourProducts, "row", productsHomeContainer)
+    } catch (error) {
+        showSwal(`${error.message}`, "error", "Refresh the page.", "../Pages/shop.html");
+    }
+}
+
+window.addEventListener("load", () => {
+    fetchGetProducts()
 })
